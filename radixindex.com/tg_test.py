c549af249e4e918eb68edad43e9be91c0ea794a2
sleeprock <40290072+sleeprock@users.noreply.github.com>
2022-06-30 23:18:23 +0300

    Update tg_test.py

--- radixindex.com/tg_test.py
+++ radixindex.com/tg_test.py
@@ -4,12 +4,12 @@ from telethon.tl.types import InputPeerUser, InputPeerChannel
 from telethon import TelegramClient, sync, events
 
 def send_msg(msg):
-    api_id = '16161285'
-    api_hash = 'b17ff295a7a8d09078ecbeb3d55456cf'
-    token = '5445633818:AAG62XdnHCyppAbzweK-L6URKlwjxh91cOk'
+    api_id = 'your_id'
+    api_hash = 'your_hash'
+    token = 'your_token'
     message = msg
 
-    phone = "+79787452726"
+    phone = "+77777777777"
 
     # creating a telegram session and assigning
     # it to a variable client
@@ -29,7 +29,7 @@ def send_msg(msg):
     try:
         # receiver user_id and access_hash, use
         # my user_id and access_hash for reference
-        receiver = InputPeerUser(195021413, 0)
+        receiver = InputPeerUser('user_id', 0)
     
         # sending message using telegram client
         client.send_message(receiver, message, parse_mode='html')
